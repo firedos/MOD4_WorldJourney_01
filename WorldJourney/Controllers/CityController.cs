11aa6769d164c8dabcd10d04b1e9b1c311ca71e1
Sergey Kiselev <fireados@outlook.com>
2020-12-06 20:18:25 +0300

    Добавьте файлы проекта.

---
+++ WorldJourney/Controllers/CityController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldJourney.Models;
+using WorldJourney.Filters;
+
+namespace WorldJourney.Controllers
+{
+    public class CityController : Controller
+    {
+
+        private IData _data;
+        private IHostingEnvironment _environment;
+
+        public CityController(IData data, IHostingEnvironment environment)
+        {
+            _data = data;
+            _environment = environment;
+            _data.CityInitializeData();
+        }
+
+        [ServiceFilter(typeof(LogActionFilterAttribute))]
+        [Route("WorldJourney")]
+        public IActionResult Index()
+        {
+            ViewData["Page"] = "Search city";
+            return View();
+        }
+        
+        [Route("CityDetails/{id?}")]
+        public IActionResult Details(int? id)
+        {
+            ViewData["Page"] = "Selected city";
+            //City city = null;
+            City city = _data.GetCityById(id);
+            if (city == null)
+            {
+                //return NotFound();
+                return Content("<h1>Параметр не задан</h1>");
+            }
+            ViewBag.Title = city.CityName;
+            return View(city);
+        }
+        public IActionResult GetImage(int? cityId)
+        {
+            ViewData["Message"] = "display Image";
+            //City requestedCity = null;
+            City requestedCity = _data.GetCityById(cityId);
+            if (requestedCity != null)
+            {
+                //string fullPath = "";
+                string webRootpath = _environment.WebRootPath;
+                string folderPath = "\\images\\";
+                string fullPath = webRootpath + folderPath + requestedCity.ImageName;
+
+                FileStream fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                byte[] fileBytes;
+                using (BinaryReader br = new BinaryReader(fileOnDisk))
+                {
+                    fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                }
+                return File(fileBytes, requestedCity.ImageMimeType);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
